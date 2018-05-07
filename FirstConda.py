43502ca5339b9c92c5353705b1ea0a2f30ffda73
Oussama <badaoui.o@gmail.com>
2018-05-07 23:46:55 +0200

    Add files via upload

---
+++ FirstConda.py
@@ -0,0 +1,14 @@
+import numpy as np
+import sklearn as sk
+
+
+class Test:
+    y = 8
+    name = "la class\n"
+    np.random
+    sk.svm
+
+    def funct(self, s):
+        print("rani ok" + s)
+
+    funct(self, "sah")
